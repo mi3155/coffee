0b4127ba72f9a228c89aef7d9362af177c6ff1cd
jihwan <hwan3155@naver.com>
2022-02-24 14:10:07 +0900

    2.23 회원가입 백엔드부분

--- src/main/java/com/example/demo/controller/commonController.java
+++ src/main/java/com/example/demo/controller/commonController.java
@@ -1,13 +1,43 @@
 package com.example.demo.controller;
 
+import com.example.demo.Entity.Coffee;
+import com.example.demo.dto.loginDTO;
+import com.example.demo.service.CoffeeServiceImp;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class commonController {
 
-    @GetMapping("coffee")
-    public String test1(){
+    @Autowired
+    final private CoffeeServiceImp CoffeeService;
+
+
+
+    @GetMapping("/coffee")
+    public String test1()
+    {
+        return "index.html";
+
+    }
+
+    @GetMapping("/Signup")
+    public String test2(){
+        return "Signup.html";
+    }
+
+    @PostMapping("/SignupProc")
+    public String test3(loginDTO dto){
+        System.out.println(dto.toString());
+        CoffeeService.CoffeeSignup(dto);
+        System.out.println("id : " + dto.getId() + "님 가입을 환영합니다.");
+
         return "index.html";
     }
+
 }
